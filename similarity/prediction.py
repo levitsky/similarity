9ed03634912c8981eb3a859afe07b18f99da3180
Lev Levitsky <lev.levitsky@phystech.su>
2026-02-17 16:46:09 +0100

    Make cache persistent and use it before prediction

--- similarity/prediction.py
+++ similarity/prediction.py
@@ -4,39 +4,55 @@ import diskcache
 from koinapy import Koina
 from .utils import Fixture
 from pyteomics import cmass
+import logging
 
 if TYPE_CHECKING:
     from .experiment import Experiment
+
+logger = logging.getLogger(__name__)
 
 
 class Index(diskcache.Index):
     """Index for predicted spectra. Uses experiment config to add collision energy, charge and model info to the key."""
 
-    def __init__(self, *args, **kwargs):
-        self.experiment = kwargs.pop("experiment")
-        super().__init__(*args, **kwargs)
+    def _full_key(self, key: str) -> tuple:
+        config = self.experiment.config
+        # key is peptide sequence, we need to add collision energy, charge and model info to the key
+        return (key, config.collision_energy, config.charge, config.model_intensity)
+
+    def __init__(self, experiment: "Experiment"):
+        self.experiment = experiment
+        super().__init__(str(experiment.config.cache_dir))
 
     def __getitem__(self, key: str) -> Any:
-        # key is peptide sequence, we need to add collision energy, charge and model info to the key
-        config = self.experiment.config
-        full_key = (key, config.collision_energy, config.charge, config.model_intensity)
+        full_key = self._full_key(key)
         return super().__getitem__(full_key)
 
     def __setitem__(self, key: str, value: Any) -> None:
-        config = self.experiment.config
-        full_key = (key, config.collision_energy, config.charge, config.model_intensity)
+        full_key = self._full_key(key)
         super().__setitem__(full_key, value)
+
+    def __contains__(self, key: str) -> bool:
+        full_key = self._full_key(key)
+        return full_key in self.cache  # direct check on Index doesn't work
 
 
 class PredictedSpectrumCollection(Fixture):
     def evaluate(self, experiment: "Experiment") -> Index:
         df = experiment.mz_irt_df
-        model = Koina(experiment.config.model_intensity, experiment.config.koina_host)
-        result = model.predict(df)
-        result.set_index("peptide_sequences", inplace=True)
         index = Index(experiment=experiment)
-        for key in df.peptide_sequences:
-            index[key] = result.loc[key, ["mz", "intensities"]].values.T
+        logger.info("Found cache with %d entries", len(index))
+        df["cached"] = df["peptide_sequences"].apply(lambda seq: seq in index)
+        logger.info("%d of %d spectra are cached", df["cached"].sum(), len(df))
+        if df["cached"].all():
+            logger.info("All spectra are cached, skipping prediction")
+            return index
+        model = Koina(experiment.config.model_intensity, experiment.config.koina_host)
+        result = model.predict(df.loc[~df["cached"]])
+        result.set_index("peptide_sequences", inplace=True)
+
+        for _, pep in df.loc[~df["cached"], "peptide_sequences"].items():
+            index[pep] = result.loc[pep, ["mz", "intensities"]].values.T
         return index
 
 
