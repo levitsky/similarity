5637b8f5b8094018265113a9af263ae809bdc0ea
Lev Levitsky <lev.levitsky@phystech.su>
2026-02-18 17:33:09 +0100

    First mp implementation

--- similarity/utils.py
+++ similarity/utils.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass, fields
 from pathlib import Path
 import argparse
+import multiprocessing as mp
+import diskcache
 from typing import Any, TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -52,3 +54,29 @@ class Config(BaseConfig):
     peak_ppm: float = 10.0
     koina_host: str = "koina.wilhelmlab.org:443"
     cache_dir: Path = Path(".")
+    workers: int = mp.cpu_count()
+
+
+class Index(diskcache.Index):
+    """Index for predicted spectra. Uses experiment config to add collision energy, charge and model info to the key."""
+
+    def _full_key(self, key: str) -> tuple:
+        config = self.experiment.config
+        # key is peptide sequence, we need to add collision energy, charge and model info to the key
+        return (key, config.collision_energy, config.charge, config.model_intensity)
+
+    def __init__(self, experiment: "Experiment"):
+        self.experiment = experiment
+        super().__init__(str(experiment.config.cache_dir))
+
+    def __getitem__(self, key: str) -> Any:
+        full_key = self._full_key(key)
+        return super().__getitem__(full_key)
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        full_key = self._full_key(key)
+        super().__setitem__(full_key, value)
+
+    def __contains__(self, key: str) -> bool:
+        full_key = self._full_key(key)
+        return full_key in self.cache  # direct check on Index doesn't work
